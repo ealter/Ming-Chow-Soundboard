a125ad75b2d186cf043f6858d6524c108e3c59c1
Music <Music@mlab8.music.tufts.edu>
2012-02-09 21:09:44 -0500

    Initial Commit

---
+++ soundBoard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  soundBoard
+//
+//  Created by Music2 on 2/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
