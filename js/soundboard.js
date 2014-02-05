4739dd276e602fd65755e8a519f7265ab4d2ce2b
Eliot Alter <Eliot.Alter@gmail.com>
2014-02-04 22:13:58 -0500

    Basic soundboard

---
+++ js/soundboard.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    var currentlyPlaying = null;
+
+    var audioDiv = document.getElementById("audioElems");
+
+    function audioTagId(elemId) {
+        return elemId + '_audio';
+    }
+
+    function createAudioTag(id) {
+        var audio = document.createElement("audio");
+        audio.src = 'sounds/' + id + ".mp3";
+        audio.id = audioTagId(id);
+        audio.preload = "auto";
+        audioDiv.appendChild(audio);
+
+        $('#' + id).click(function() {
+            if(currentlyPlaying != null) {
+                currentlyPlaying.pause();
+            }
+            console.log(audio);
+            if(audio.readyState >= 1) {
+                audio.currentTime = 0;
+                audio.pause();
+            }
+            audio.play();
+            currentlyPlaying = audio;
+        });
+    }
+
+    $(".soundboardItem").each(function(index, elem) {
+        createAudioTag(elem.id);
+    });
+});
+    console.log("foo");
+
